86650c0678bebc096831a00c289268cc6a139b44
yravindraswamy <yravindraswamy@gmail.com>
2023-09-09 19:21:45 +0530

    html

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,113 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="sass-heading-container">
+    <div class="sass-heading-container-card">Ravindra swamy</div>
+  </div>
+  <div class="data-table-container">
+    <!-- <table>
+      <thead>
+        <tr>
+          <th>S.No</th>
+          <th>Initial</th>
+          <th>FirstName</th>
+          <th>LastName</th>
+          <th>Education</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="person in persons" :key="person.sno">
+          <td>
+            {{ person.sno }}
+          </td>
+          <td>
+            {{ person.initial }}
+          </td>
+          <td>
+            {{ person.FirstName }}
+          </td>
+          <td>
+            {{ person.lastName }}
+          </td>
+          <td>
+            {{ person.education }}
+          </td>
+        </tr>
+      </tbody>
+    </table> -->
+    <div class="first">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, ipsum?
+    </div>
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+      accusantium cumque aut quis ut sed animi, enim, optio impedit odio
+      deleniti ipsam maxime doloremque voluptates omnis similique, at corrupti!
+      Vero pariatur expedita corrupti odit aut ex, ducimus natus perferendis
+      laudantium asperiores minima accusantium voluptates impedit tenetur
+      aliquid fuga error illum exercitationem quidem velit, eveniet excepturi
+      cum iure sint. Eius, exercitationem! Soluta quo aliquid repudiandae
+      dignissimos excepturi quas ullam quae, sit eveniet aut, voluptatibus
+      blanditiis recusandae cum ipsam obcaecati? Eligendi, molestiae assumenda.
+      Ad nemo perspiciatis voluptatem itaque perferendis, maxime enim maiores
+      cupiditate dignissimos quos asperiores nesciunt! Aliquid, rerum sint. Sed
+      excepturi, mollitia vero alias sunt nostrum natus velit quaerat, possimus
+      animi, dolor dolores voluptates culpa corporis magni quis! Quam
+      necessitatibus natus, amet cumque deserunt rem maiores ipsam consequatur
+      error, incidunt doloremque blanditiis unde ullam inventore aliquam ratione
+      quidem? Accusamus voluptatum sit dicta laudantium magni vero iste quia
+      quam adipisci impedit dolorum, accusantium ipsam consequatur
+      exercitationem assumenda temporibus aspernatur odit dolore enim nesciunt
+      perspiciatis laborum consectetur? Magni dolore minima eum libero aliquid
+      dolor exercitationem dolorem soluta, quas illo itaque sed atque cum
+      mollitia. Quod sapiente sint molestias voluptatum nihil deserunt fuga
+      quidem quisquam aliquid veritatis repellat, reprehenderit, magni
+      consectetur. Laudantium, fuga officiis.
+    </div>
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi hic optio
+      amet suscipit, vel modi odit numquam consectetur, possimus aspernatur
+      explicabo accusantium perferendis iure neque fugit eos ratione architecto
+      non dicta, ullam pariatur quisquam nulla dignissimos esse! Earum inventore
+      dolor, nobis quam iure cum. Placeat earum soluta asperiores iste mollitia
+      hic ex omnis, debitis amet quibusdam eligendi pariatur molestias ratione
+      natus dolore. Omnis, rerum? Vitae nulla amet, exercitationem nam voluptate
+      sit maxime eum? Consequuntur asperiores, architecto dolorem quibusdam
+      molestias modi nisi cupiditate consequatur amet voluptatibus tempore
+      expedita impedit fugiat ut quaerat ducimus veniam incidunt facilis
+      voluptatum atque officiis. Odio temporibus, vero officia, vitae sapiente
+      molestiae vel ipsam placeat fugiat qui cum reprehenderit asperiores,
+      corrupti tempora ipsa obcaecati nobis. Perferendis in dolorem impedit
+      nesciunt dignissimos dolores ullam culpa rerum, fugiat dolorum,
+      exercitationem iusto architecto cupiditate necessitatibus nam a voluptatem
+      quidem repellat. Nisi hic accusamus libero iusto et modi illo deserunt,
+      inventore sunt, tempora, mollitia sapiente sed sit adipisci omnis. Minus
+      nisi repellat cum ipsum saepe, quidem libero sed laborum distinctio harum
+      culpa beatae debitis? Alias obcaecati ullam voluptatum fuga laudantium
+      esse corporis explicabo iusto tempora, atque eos quas repellat tenetur,
+      hic expedita temporibus numquam ipsa facere, natus voluptatibus? Iste
+      doloribus voluptatum quia officiis itaque, quisquam atque aspernatur qui
+      optio, delectus recusandae enim accusantium doloremque nesciunt aperiam
+      voluptas eligendi alias, similique nihil! Repellat tempore cum assumenda
+      ullam asperiores, soluta minima expedita voluptate placeat aperiam saepe
+      aut magnam natus voluptates reiciendis? Soluta, facilis. Quam est sunt qui
+      modi sapiente maiores odio cumque iure aliquid obcaecati temporibus eos,
+      natus facere atque quis. Temporibus suscipit a delectus accusamus nihil
+      aperiam ratione illo, ab quas expedita quibusdam, eos sapiente praesentium
+      dignissimos sunt? Officiis, ducimus officia totam culpa eaque reiciendis
+      quasi ratione, veritatis ipsum odio ipsa quas alias tenetur itaque neque
+      quidem non consectetur unde! Excepturi, voluptatem.
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import persons from "./assets/persons.json";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      persons,
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
